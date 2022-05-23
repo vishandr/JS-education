0920a664dac8fb5b19f6d9358a63bd3d6f58242f
vishand <vishandr@gmail.com>
2022-05-23 18:09:57 +0300

    experimented with squares and lines

--- canvas.js
+++ canvas.js
@@ -3,13 +3,16 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 let position = 0;
 let x = 100;
-let y = 100;
+let y = 20;
+
+
 
 setInterval (function(){
     ctx.clearRect(0, 0, 200, 200);
     ctx.fillRect(x, position, 10, 10);
+    document.getElementById('canvas').getContext('2d').fillRect(position, y, 10, 10)
     position ++;
-    if( position > 199){
+    if( position > 200){
         position = 0;
         x = Math.floor(Math.random()*200);
     }
@@ -18,12 +21,22 @@ setInterval (function(){
 
 setInterval (function(){
     document.getElementById('canvas1').getContext('2d').clearRect(0, 0, 200, 200);
-    document.getElementById('canvas1').getContext('2d').fillRect(0, y, position, 10)
+    document.getElementById('canvas1').getContext('2d').fillRect( x, position, 10, 55)
+    document.getElementById('canvas1').getContext('2d').fillRect( 0, x, position, 5)
     position ++;
-    if( position > 199){
+    if( position > 200){
         position = 0;
         y = Math.floor(Math.random()*200);
     }
 }, 10);
-// console.log(canvas)
 
+
+setInterval (function(){
+    document.getElementById('canvas2').getContext('2d').clearRect(0, 0, 200, 200);
+    document.getElementById('canvas2').getContext('2d').fillRect( 0, 0, position, position)
+    position ++;
+    if( position > 200){
+        position = 0;
+        y = Math.floor(Math.random()*200);
+    }
+}, 24);
