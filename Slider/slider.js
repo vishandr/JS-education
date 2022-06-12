a165304fd36febf348c211e6916066b7b2c02fd7
vishandr <101496759+vishandr@users.noreply.github.com>
2022-06-12 12:09:48 +0300

    amend: changed px to % (made responsive)

--- Slider/slider.js
+++ Slider/slider.js
@@ -2,13 +2,13 @@ let offset = 0 // смещение от левого края
 const sliderLine = document.querySelector('.slider-line')
 
 document.querySelector('.slider-next').addEventListener('click', function(){
-    offset -= 600;
-    if (offset < -1800) offset = 0;
-    sliderLine.style.left = offset + 'px';
+    offset -= 100;
+    if (offset < -300) offset = 0;
+    sliderLine.style.left = offset + '%';
 })
 
 document.querySelector('.slider-prev').addEventListener('click', function(){
-    offset += 600;
-    if (offset > 0) offset = -1800;
-    sliderLine.style.left = offset + 'px';
+    offset += 100;
+    if (offset > 0) offset = -300;
+    sliderLine.style.left = offset + '%';
 })
